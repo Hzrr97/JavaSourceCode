400577a4b7aa34d7d55e33d38c65d7382ee3f5dd
huangzhuoran <huangzhuoran@haomo-studio.com>
2024-03-19 09:06:11 +0800

    feat: 添加源码

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JavaSourceCode
 Java源码分析：JDK源码、Spring源码、SpringBoot源码、SpringAOP源码、SpringSecurity源码、SpringSecurity OAuth2源码、Netty源码
+
+该仓库只保留源代码不做任何修改和笔记
---
+++ jdk8/src/test/collection/TestList.java
@@ -0,0 +1,11 @@
+package test.collection;
+
+
+import java.util.ArrayList;
+
+// 创建一维数组
+public class TestList {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+    }
+}

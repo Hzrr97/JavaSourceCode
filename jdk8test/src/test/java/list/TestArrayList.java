5b8cf6066530a0f014c9ecc1ea61b4cc661bb7c5
huangzhuoran <huangzhuoran@haomo-studio.com>
2024-03-19 10:50:54 +0800

    feat: 添加测试工程

---
+++ jdk8test/src/test/java/list/TestArrayList.java
@@ -0,0 +1,18 @@
+package list;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class TestArrayList {
+
+    @Test
+    public void testAdd() {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("sss");
+    }
+
+
+}
